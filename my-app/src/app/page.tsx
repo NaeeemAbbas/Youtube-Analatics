09aa36e1db5648a1b09b0ac6a5b9663b85af8568
Naeem Abbas <150218715+NaeeemAbbas@users.noreply.github.com>
2025-10-23 12:14:17 +0500

    Changes in the main

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
             Channel 3
           </h2>
           <p className="text-sm text-gray-500">
-            View detailed analytics and .
+            View detailed analytics and stats.
           </p>
         </div>
 
